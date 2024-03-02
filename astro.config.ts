7a25ec6f5db1461c845aa2ab2fc0fdcbbae7a803
David Matos <dmatos@aischannel.com>
2024-03-02 13:18:18 +0100

    remove package-lock.json

---
+++ astro.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless"
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  adapter: vercel({
+		webAnalytics: { enabled: true },
+	}),
+  output: "hybrid",
+});
