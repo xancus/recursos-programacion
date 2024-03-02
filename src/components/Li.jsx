7a29e8c8672322e0d6ce074e4e44e400e303ed04
David Matos <dmatos@aischannel.com>
2024-03-02 21:46:34 +0100

    remove unnecessary jsx component

--- src/components/Li.jsx
+++ src/components/Li.jsx
@@ -1,18 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 
-export function Li ({ slug="", classAttr="", children}) {
-    const [extraClass, setExatraClass] = useState('')
-    
-    useEffect(() => {
-        if(window.location.pathname.split('/')[1] === slug) setExatraClass('bg-primary text-secondary')
-    }, [])
-    
- 
-    return (
-        <a href={`/${slug.toLowerCase()}`}>
-            <li data-label={slug} className={`${classAttr ? classAttr : ' hover:bg-hover-primary hover:cursor-pointer hover:text-secondary flex items-center gap-2 font-semibold px-4 mb-1 py-4 rounded-lg duration-100'} ${extraClass}`}>
-                {children}
-            </li>
-        </a>
-    )
+export function Li ({ slug = '', classAttr = '', children }) {
+  const [extraClass, setExatraClass] = useState('')
+
+  useEffect(() => {
+    if (window.location.pathname.split('/')[1] === slug) setExatraClass('bg-primary text-secondary')
+  }, [])
+
+  return (
+    <a href={`/${slug.toLowerCase()}`}>
+      <li data-label={slug} className={`${classAttr || ' hover:bg-hover-primary hover:cursor-pointer hover:text-secondary flex items-center gap-2 font-semibold px-4 mb-1 py-4 rounded-lg duration-100'} ${extraClass}`}>
+        {children}
+      </li>
+    </a>
+  )
 }
