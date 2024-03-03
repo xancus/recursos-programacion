5810ed77a7149f5b9bf2e604440bc57e6b5883a3
David Matos <dmatos@aischannel.com>
2024-03-03 16:21:06 +0100

    Some style code adjustments

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'primary': 'var(--color-primary)',
-				'secondary': 'var(--color-secondary)',
-				'hover-primary': 'var(--hover-color-primary)',
-				'hover-secondary': 'var(--hover-color-secondary)'
-			}
-		},
-	},
-	darkMode: 'class',
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        'hover-primary': 'var(--hover-color-primary)',
+        'hover-secondary': 'var(--hover-color-secondary)'
+      }
+    }
+  },
+  darkMode: 'class',
+  plugins: []
 }
