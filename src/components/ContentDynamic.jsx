4bd59f9ebb4b5126503162c94425c6aa0a92102a
David Matos <davidxanco@gmail.com>
2024-11-09 18:24:44 +0100

    Fix placeholder when selecting

--- src/components/ContentDynamic.jsx
+++ src/components/ContentDynamic.jsx
@@ -5,6 +5,7 @@ import { CardBlock } from './CardBlock'
 export function ContentDynamic ({ titleSection }) {
   const [currentContent, setCurrentContent] = useState([])
   const [showFilter, setShowFilter] = useState(false)
+  const [palceholder, setPlaceholder] = useState('Filter by')
 
   useEffect(() => {
     if (titleSection === 'all') {
@@ -17,13 +18,14 @@ export function ContentDynamic ({ titleSection }) {
 
   function handleClick (ev) {
     const name = ev.target.getAttribute('data-class')
+    setPlaceholder(name.charAt(0).toUpperCase() + name.slice(1))
     if (name === 'all') {
       setCurrentContent(contentArray)
       setShowFilter(!showFilter)
       return
     }
 
-    const filteredContent = currentContent.filter(cont => cont.type === name)
+    const filteredContent = contentArray.filter(cont => cont.type === name && cont.category === titleSection)
     setCurrentContent(filteredContent)
     setShowFilter(!showFilter)
   }
@@ -37,10 +39,10 @@ export function ContentDynamic ({ titleSection }) {
             onClick={() => setShowFilter(!showFilter)}
             className='py-1 px-2 md:py-2 md:px-4 border border-primary rounded-lg hover:bg-hover-primary hover:text-secondary relative flex flex-row items-center gap-2'
           >
-            Filter by <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='30' height='30'><path fill='none' d='M0 0h24v24H0z' /><path fill='currentColor' d='M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z' /></svg>
+            {palceholder} <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='30' height='30'><path fill='none' d='M0 0h24v24H0z' /><path fill='currentColor' d='M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z' /></svg>
 
           </button>
-          <ul className={`${showFilter ? 'block' : 'hidden'} h-auto text-primary text-xl border border-[white] rounded-lg w-full p-2 min-w-[150px] absolute bg-[var(--background-color)] top-11`}>
+          <ul className={`${showFilter ? 'block' : 'hidden'} z-50 h-auto text-primary text-xl border border-[white] rounded-lg w-full p-2 min-w-[150px] absolute bg-[var(--background-color)] top-11`}>
             <li
               data-class='all'
               onClick={handleClick}
